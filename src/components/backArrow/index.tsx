13ddb35c302ea270f9fdf0f65a7240692c107fed
Naahueeh <nahuelramos518@gmail.com>
2026-02-04 07:29:54 -0300

    Ajuste de footer y flecha de navegación

--- src/components/backArrow/index.tsx
+++ src/components/backArrow/index.tsx
@@ -1,11 +1,21 @@
 import Image from 'next/image';
 import ArrowSvg from '../images/back-arrow.svg';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 const BackArrow = ({ href }: any) => {
+  const [enlace, setEnlace] = useState(href);
+
+  useEffect(() => {
+    if(sessionStorage.getItem("vineDeHomeRojo") == 'si'){
+      setEnlace('/')
+    }
+  }, []); 
+
   return (
     <div>
-      <Link href={href}>
+      <Link href={enlace}>
         <Image src={ArrowSvg} alt="back arrow" width={25} height={25} />
       </Link>
     </div>
